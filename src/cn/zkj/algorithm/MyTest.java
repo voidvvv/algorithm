00bec9e682734ab5e6f960c3f4e90b16049bf8d4
kj.zhao <kj.zhao@trip.com>
2021-11-11 13:23:46 +0800

    test class

---
+++ src/cn/zkj/algorithm/MyTest.java
@@ -0,0 +1,11 @@
+package cn.zkj.algorithm;
+
+/**
+ * Author: KJ.ZHAO
+ * Date: 2021/11/11 13:23
+ */
+public class MyTest {
+    public static void main(String[] args) {
+        System.out.println("MyTest");
+    }
+}
