6775377a68d24270ba25418c790e150788a5245a
kj.zhao <kj.zhao@trip.com>
2021-11-11 13:46:07 +0800

    link https://gitee.com/voidvvv/algorithm/issues/I4HU3R

--- src/cn/zkj/algorithm/MyTest.java
+++ src/cn/zkj/algorithm/MyTest.java
@@ -7,5 +7,8 @@ package cn.zkj.algorithm;
 public class MyTest {
     public static void main(String[] args) {
         System.out.println("MyTest");
+
+
+        System.out.println("link issue I4HU3R url!! !!!");
     }
 }
