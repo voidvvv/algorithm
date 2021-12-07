db945a5520579b4ad518732973cc037d98461aea
zkj <892925673@qq.com>
2021-12-07 22:38:29 +0800

    update

--- src/cn/zkj/lk/Solution2.java
+++ src/cn/zkj/lk/Solution2.java
@@ -1506,4 +1506,44 @@ public class Solution2 {
         return re;
     }
 
+
+    /**
+     * 383. 赎金信
+     * 为了不在赎金信中暴露字迹，从杂志上搜索各个需要的字母，组成单词来表达意思。
+     *
+     * 给你一个赎金信 (ransomNote) 字符串和一个杂志(magazine)字符串，判断 ransomNote 能不能由 magazines 里面的字符构成。
+     *
+     * 如果可以构成，返回 true ；否则返回 false 。
+     *
+     * magazine 中的每个字符只能在 ransomNote 中使用一次。
+     *
+     *  
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/ransom-note
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     * @param ransomNote
+     * @param magazine
+     * @return
+     */
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] ransomNoteArr = new int[26];
+        char[] chars = ransomNote.toCharArray();
+        char[] chars1 = magazine.toCharArray();
+
+        for (int x=0;x<chars.length;x++){
+            ransomNoteArr[chars[x]-'a']++;
+        }
+        for (int x=0;x<chars1.length;x++){
+            ransomNoteArr[chars1[x]-'a']--;
+        }
+
+        for (int x=0;x<26;x++){
+            if (ransomNoteArr[x]>0){
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
