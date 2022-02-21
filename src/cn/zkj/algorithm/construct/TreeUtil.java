770bcf7229a6f75253740a1797a9ba81a77d1c34
zkj <892925673@qq.com>
2022-02-21 21:58:04 +0800

    红黑树

---
+++ src/cn/zkj/algorithm/construct/TreeUtil.java
@@ -0,0 +1,60 @@
+package cn.zkj.algorithm.construct;
+
+import cn.zkj.algorithm.construct.listnode.Node;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @Classname TreeUtil
+ * @Description
+ * @Date 2022/2/21 21:54
+ * @Created by zkj
+ */
+public class TreeUtil {
+
+
+    static public void traverse(Node root){
+        Queue<Node> queue = new LinkedList<>();
+        Queue<Node> queue2 = new LinkedList<>();
+        queue.offer(root);
+        int x=1;
+
+        while (true){
+            boolean f = true;
+            StringBuilder print = new StringBuilder(String.format("第%s层",x++));
+            if (!queue.isEmpty()){
+                while (!queue.isEmpty()){
+                    Node poll = queue.poll();
+                    if (poll==null){
+                        print.append("null\t");
+                    }else {
+                        f= false;
+                        print.append(poll.data);
+                        print.append(" ");
+                        print.append(poll.red).append("\t");
+                        queue2.offer(poll.left);
+                        queue2.offer(poll.right);
+                    }
+                }
+            }else if (!queue2.isEmpty()){
+                while (!queue2.isEmpty()){
+                    Node poll = queue2.poll();
+                    if (poll==null){
+                        print.append("null\t");
+                    }else {
+                        f= false;
+                        print.append(poll.data);
+                        print.append(" ");
+                        print.append(poll.red).append("\t");
+                        queue.offer(poll.left);
+                        queue.offer(poll.right);
+                    }
+                }
+            }
+
+            System.out.println(print.toString());
+        }
+
+    }
+}
