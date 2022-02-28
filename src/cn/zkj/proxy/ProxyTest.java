8414eb7cf67dd73c5636bd466073eda4e116ec1b
kj.zhao <kj.zhao@trip.com>
2022-02-28 14:45:21 +0800

    proxy

--- src/cn/zkj/proxy/ProxyTest.java
+++ src/cn/zkj/proxy/ProxyTest.java
@@ -5,17 +5,31 @@ import cn.zkj.proxy.dto.MyTest;
 import cn.zkj.proxy.dto.MyTestInterFace;
 import cn.zkj.proxy.handler.InvocationHandlerImpl;
 import cn.zkj.proxy.handler.NoObjInvocationHandler;
+import sun.misc.ProxyGenerator;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Proxy;
+import java.util.Arrays;
 
 /**
  * Author: KJ.ZHAO
  * Date: 2022/2/28 14:16
  */
 public class ProxyTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         MyOnlyInterFace o =(MyOnlyInterFace) Proxy.newProxyInstance(ProxyTest.class.getClassLoader(), new Class[]{MyOnlyInterFace.class}, new NoObjInvocationHandler());
         o.only();
+        InvocationHandler invocationHandler = Proxy.getInvocationHandler(o);
+        System.out.println(invocationHandler);
+
+        byte[] myvoids = ProxyGenerator.generateProxyClass("myvoid", new Class[]{MyTestInterFace.class});
+
+        FileOutputStream fos = new FileOutputStream("myvoids");
+        fos.write(myvoids);
+        System.out.println(Arrays.toString(myvoids));
     }
 
     public static void test01(){
@@ -25,6 +39,9 @@ public class ProxyTest {
 
         MyTestInterFace o =(MyTestInterFace) Proxy.newProxyInstance(myTest.getClass().getClassLoader(), myTest.getClass().getInterfaces(), handler);
 
+
         System.out.println(o.say());
+
+
     }
 }
